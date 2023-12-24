4fcc073f12d028eebeae383d83b5e8f69db71561
YimTaekeun <ytk030305@gmail.com>
2023-12-24 22:48:59 +0900

    Feat: true false에 따른 요소 바꾸기 추가

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,14 +1,20 @@
 import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 const Page2 = () =>{
     const[searchParams, setSearchParams] = useSearchParams();
+    const[test, setTest] = useState(false);
     const tag = searchParams.get("hello");
+    // let test = false;
+    function changeTest(){setTest(!test);}
     return <div className="Page2">
         {
-        (tag == "Hello") ?
+        (test) ?
         <h1>This page is Page2</h1>
         :
         <h2>Fail....</h2>
         }
+        <button onClick={() => {setTest(!test);}}>clickToChangeTest</button>
+        
     </div>
 }
 export default Page2;
